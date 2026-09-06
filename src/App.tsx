f69815766e00cac050a31914dbd6f452cf299ec7
hardresetfibercraft-commits <hardresetfibercraft@gmail.com>
2026-09-06 18:42:37 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import { CartProvider } from './lib/cart';
 import { StoreProvider } from './lib/store';
 import { Tip4ServAuthProvider } from './lib/tip4servAuth';
 import { ThemeProvider } from './lib/theme';
 import { ToastProvider } from './lib/toast';
 import { LanguageProvider } from './lib/i18n';
+
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
+
 import CartDrawer from './components/cart/CartDrawer';
+
 import ToastContainer from './components/ui/ToastContainer';
+import AnnouncementBar from './components/ui/AnnouncementBar';
+
 import HomePage from './pages/HomePage';
 import ProductsPage from './pages/ProductsPage';
 import ProductDetailPage from './pages/ProductDetailPage';
@@ -23,35 +29,96 @@ export default function App() {
   return (
     <BrowserRouter>
       <ThemeProvider>
+
         <LanguageProvider>
-        <ToastProvider>
-          <StoreProvider>
-            <Tip4ServAuthProvider>
-              <CartProvider>
-                <div className="min-h-screen flex flex-col">
-                  <Header />
-                  <main className="flex-1">
-                    <Routes>
-                      <Route path="/" element={<HomePage />} />
-                      <Route path="/products" element={<ProductsPage />} />
-                      <Route path="/product/:slug" element={<ProductDetailPage />} />
-                      <Route path="/checkout" element={<CheckoutPage />} />
-                      <Route path="/checkout/success" element={<CheckoutSuccessPage />} />
-                      <Route path="/checkout/canceled" element={<CheckoutCanceledPage />} />
-                      <Route path="/account" element={<AccountPage />} />
-                      <Route path="/auth/discord/callback" element={<DiscordOAuthCallbackPage />} />
-                      <Route path="/admin" element={<AdminPage />} />
-                    </Routes>
-                  </main>
-                  <Footer />
-                  <CartDrawer />
-                  <ToastContainer />
-                </div>
-              </CartProvider>
-            </Tip4ServAuthProvider>
-          </StoreProvider>
-        </ToastProvider>
+
+          <ToastProvider>
+
+            <StoreProvider>
+
+              <Tip4ServAuthProvider>
+
+                <CartProvider>
+
+                  <div className="min-h-screen flex flex-col">
+
+                    {/* GLOBAL ANNOUNCEMENT BAR */}
+                    <AnnouncementBar />
+
+                    {/* MAIN NAVIGATION */}
+                    <Header />
+
+                    {/* PAGE CONTENT */}
+                    <main className="flex-1">
+                      <Routes>
+
+                        <Route
+                          path="/"
+                          element={<HomePage />}
+                        />
+
+                        <Route
+                          path="/products"
+                          element={<ProductsPage />}
+                        />
+
+                        <Route
+                          path="/product/:slug"
+                          element={<ProductDetailPage />}
+                        />
+
+                        <Route
+                          path="/checkout"
+                          element={<CheckoutPage />}
+                        />
+
+                        <Route
+                          path="/checkout/success"
+                          element={<CheckoutSuccessPage />}
+                        />
+
+                        <Route
+                          path="/checkout/canceled"
+                          element={<CheckoutCanceledPage />}
+                        />
+
+                        <Route
+                          path="/account"
+                          element={<AccountPage />}
+                        />
+
+                        <Route
+                          path="/auth/discord/callback"
+                          element={<DiscordOAuthCallbackPage />}
+                        />
+
+                        <Route
+                          path="/admin"
+                          element={<AdminPage />}
+                        />
+
+                      </Routes>
+                    </main>
+
+                    {/* FOOTER */}
+                    <Footer />
+
+                    {/* GLOBAL OVERLAYS */}
+                    <CartDrawer />
+                    <ToastContainer />
+
+                  </div>
+
+                </CartProvider>
+
+              </Tip4ServAuthProvider>
+
+            </StoreProvider>
+
+          </ToastProvider>
+
         </LanguageProvider>
+
       </ThemeProvider>
     </BrowserRouter>
   );
